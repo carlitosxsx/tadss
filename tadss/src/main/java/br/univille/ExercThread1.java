b6d571c46af163b6acfc42d3f9e62684af3ec1e7
carlitosxsx <91803846+carlitosxsx@users.noreply.github.com>
2026-03-12 01:31:35 +0000

    feat

--- tadss/src/main/java/br/univille/ExercThread1.java
+++ tadss/src/main/java/br/univille/ExercThread1.java
@@ -1,8 +1,19 @@
 package br.univille;
 
+import java.util.Date;
+
 public class ExercThread1 /*extends Thread*/ implements Runnable{
-    @Override
     public void run() {
             System.out.println(/*getName() + " - " + new Date()*/ Thread.currentThread().getName() + " - " + new Date());
+    }
+
+    public static void main(String[] args) {
+        // Faça um programa concorrente que crie 10 threads que exibam o nome da thread e a hora atual.
+        for (int i = 1; i <= 10; i++) {
+            ExercThread1 t1 = new ExercThread1();
+            Thread.ofPlatform().start(t1); /*Java 19*/
+            /*new Thread(t1).start();*/
+            /*t1.start();*/
+        }
     }
 }
--- tadss/src/main/java/br/univille/ExercThread2.java
+++ tadss/src/main/java/br/univille/ExercThread2.java
@@ -1,13 +1,9 @@
 package br.univille;
 
 public class ExercThread2 /*extends Thread*/ implements Runnable{
-    @Override
     public void run() {
         for (int i = 1; i <= 10; i++) {
             System.out.println(Thread.currentThread().getName() + " - Contador: " + i);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {}
         }
     }
 
@@ -15,10 +11,20 @@ public class ExercThread2 /*extends Thread*/ implements Runnable{
     /*public void run() {
         for (int i = 0; i < 5; i++) {
             System.out.println(getName() + " - Número: "+ i);
+        }
+    }*/
+    
+    public static void main(String[] args) {
+        // Faça um programa concorrente que crie 10 threads que exibam o nome da thread e um contador que varia de 1 a 10. Execute várias vezes e observe as variações na saída. Os valores são sempre impressos na mesma ordem?
+        ExercThread2 t2 = new ExercThread2();
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            threads[i] = Thread.ofVirtual().name("Thread-" + i).start(t2); /*Java 19*/
+        }
+        for (int i = 0; i < 10; i++) {
             try {
-                Thread.sleep(100);
+                threads[i].join();
             } catch (InterruptedException e) {}
         }
     }
-    */
 }
